34e6538a999172b7f636e2628de079429e9ac872
Samu412 <samruddhihsabale@gmail.com>
2025-10-29 09:56:31 +0530

    Initial commit for jewelry website

---
+++ Jewellery_website.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector(".custom-navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
